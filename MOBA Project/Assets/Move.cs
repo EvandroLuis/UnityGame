88451befadf627c95b326be8767db252a259adaf
Evandro Luis <evandroluis58@gmail.com>
2018-05-21 21:23:47 -0300

    New Map
    Mudanças para o mapa com todos os componentes

---
+++ MOBA Project/Assets/Move.cs
@@ -0,0 +1,157 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float moveSpeed = 50f;
+    public float turnSpeed = 50f;
+
+
+    bool isTouchDevice;
+
+    //Speed
+    float arrowMouseSpeed = 10.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        if (SystemInfo.deviceType == DeviceType.Handheld)
+        {
+            isTouchDevice = true;
+        }
+        else
+        {
+            isTouchDevice = false;
+        }
+
+        //local de rotação
+        Vector3 rot = transform.localRotation.eulerAngles;
+        rotY = rot.y;
+        rotX = rot.x;
+
+    }
+
+
+    void Update()
+    {
+        var posicao = transform.position;
+
+
+        //matriz da posicao
+        Vector3 localRight = new Vector3();
+        localRight.x = transform.worldToLocalMatrix.m00;
+        localRight.y = transform.worldToLocalMatrix.m01;
+        localRight.z = transform.worldToLocalMatrix.m02;
+
+        Vector3 localFront = new Vector3();
+        localFront.x = transform.worldToLocalMatrix.m20;
+        localFront.y = transform.worldToLocalMatrix.m21;
+        localFront.z = transform.worldToLocalMatrix.m22;
+
+        //movendo a camera
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += localRight * (-0.5f);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += localFront * (0.5f);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += localFront * (-0.5f);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += localRight * (0.5f);
+        }
+
+
+
+        if (isTouchDevice)
+        {
+
+            cameraRotationAccelerometer();
+        }
+        else
+        {
+            //rotação camera e do mouse
+            moveWithArrowAndMouse();
+        }
+    }
+
+
+    float xValue;
+    float xValueMinMax = 50.0f;
+
+    float yValue;
+    float yValueMinMax = 50.0f;
+
+    float cameraSpeed = 20.0f;
+    Vector3 accelometerSmoothValue;
+
+    void cameraRotationAccelerometer()
+    {
+        //Set X Min Max
+        if (xValue < -xValueMinMax)
+            xValue = -xValueMinMax;
+
+        if (xValue > xValueMinMax)
+            xValue = xValueMinMax;
+
+        //Set Y Min Max
+        if (yValue < -yValueMinMax)
+            yValue = -yValueMinMax;
+
+        if (yValue > yValueMinMax)
+            yValue = yValueMinMax;
+
+        accelometerSmoothValue = lowpass();
+
+        xValue += accelometerSmoothValue.x;
+        yValue += accelometerSmoothValue.y;
+
+        transform.rotation = new Quaternion(yValue, xValue, 0, cameraSpeed);
+    }
+
+    //Smooth Accelerometer
+    public float AccelerometerUpdateInterval = 1.0f / 100.0f;
+    public float LowPassKernelWidthInSeconds = 0.001f;
+    public Vector3 lowPassValue = Vector3.zero;
+    Vector3 lowpass()
+    {
+        float LowPassFilterFactor = AccelerometerUpdateInterval / LowPassKernelWidthInSeconds;//tweakable
+        lowPassValue = Vector3.Lerp(lowPassValue, Input.acceleration, LowPassFilterFactor);
+        return lowPassValue;
+    }
+
+    //Movimento teclado e mouse
+    void moveWithArrowAndMouse()
+    {
+
+
+        //Keyboard Mouse
+        moveCamera(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"), arrowMouseSpeed);
+    }
+
+    //Movimentos paramentos
+    float mouseX;
+    float mouseY;
+    Quaternion localRotation;
+    private float rotY = 0.0f; // rotation around the up/y axis
+    private float rotX = 0.0f; // rotation around the right/x axis
+
+    void moveCamera(float horizontal, float verticle, float moveSpeed)
+    {
+        mouseX = horizontal;
+        mouseY = -verticle;
+
+        rotY += mouseX * moveSpeed;
+        rotX += mouseY * moveSpeed;
+
+        localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
+        transform.rotation = localRotation;
+    }
+}
---
+++ MOBA Project/Assets/[1]Avatares/Mage/README/readme_en.txt
@@ -0,0 +1,58 @@
+﻿=======================================
+Yukari Yuzuki - ver.2 packaging
+Version: 2.2
+Created by kummd
+=======================================
+
+Thank you for downloading my model!!
+
+
+@@@ Information of morph @@@
+ +Name of morph is supported english(Please tell me better name...)
+ +If you use "Shoes off", you should move "全ての親" to down.
+ +Some morph related to other morph. (Star+、Tear Big、Sweat Big etc...)
+ +Some support to the AutLuminus.
+ >>>Please read 『せつめいしょ.txt』 as detail.(Sorry detail is japanese only...)
+
+
+If you find some problem, please tell me following ways
+Twitter -> @kummd
+Mail -> kuma@kummd.com
+
+
+<UPDATE HISTORY>
+2014/04/01 ver.2.2   Update hair and nekomimi(Cat ear)
+2014/01/11 ver.2.1.3 Correct weight on neck.
+2013/07/29 ver.2.1.2 Correct IK setting for Blazer and Summer models.
+2013/05/18 ver.2.1.1 Correct joint setting for "腰モジュールbase"
+2013/01/31 ver.2.1.0 Add/Modify/Correct Morph and correct weight at crotch
+2013/01/10 ver.2.0.7 Morph view was arranged
+2013/01/08 ver.2.0.6 Morph was added/corrected
+2013/01/08 ver.2.0.5 Bugfix
+2013/01/08 ver.2.0.4 Release default yukari model ve.r2.0! and summer/blazer model bugfix.
+2012/12/16 ver.2.0.3 Bugfix
+2012/12/13 ver.2.0.2 Bugfix
+2012/12/06 ver.2.0 Summer and Blazer costume ver new
+
+<TERMS OF USE>
+I permit you to re-distribute the remodeling data. And please inform me that one.
+You must attatch readme file of same content.
+This model makes VOCALOID and VOCALOID2 amotif.
+So, please use it according to the character use guidline that CRYPTON FUTURE MEDIA, INC and INTERNET Co., Ltd. present.
+
+The TERMS OF USE will be subjected to change in the future. 
+In this case, it applies newest  TERMS OF USE.
+
+
+<PROHIBITION MATTER>
+ +Use in contradiction to public common good
+ +Use to slander it to specific individual and group
+ +Use to adult contents
+ +Commercial use
+
+<EXEMPTION FROM RESPONSIBILITY>
+I do not assume the responsibility to damage because of the use of this data at all. 
+
+**********************************************
+As for this, if english contents is different to japanese contents, Japanese is given to priority. 
+**********************************************
